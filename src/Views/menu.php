10f697f070dd9a486f3f8581f9e816a6e1d9e657
Isaac Machakata <isaac@caasi.co.zw>
2023-12-06 00:56:15 +0200

    refactor: menu authentication

--- src/Views/menu.php
+++ src/Views/menu.php
@@ -1,15 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom border-success sticky-top shadow-sm">
+<nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top shadow-sm">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="/"><span class="text-success">Hostel</span><span class="text-muted">ia</span></a>
+        <a class="navbar-brand fw-bold" href="/">Home</a>
         <button class="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link<?= url_active('/') ?>" aria-current="page" href="/">Home</a>
-                </li>
-                <?php if (session('UserType') == 'Admin') : ?>
+                <?php if (!user()->is_student) : ?>
                     <li class="nav-item">
                         <a class="nav-link<?= url_active('/booked-rooms.php') ?>" href="/booked-rooms.php">View Bookings</a>
                     </li>
@@ -17,7 +14,7 @@
                         <a class="nav-link<?= url_active('/manage-rooms.php') ?>" href="/manage-rooms.php">Manage Rooms</a>
                     </li>
                 <?php endif; ?>
-                <?php if (session('UserID')) : ?>
+                <?php if (user()->getId()) : ?>
                     <li class="nav-item">
                         <a class="nav-link<?= url_active('/my-account.php') ?>" href="/my-account.php">My Account</a>
                     </li>
