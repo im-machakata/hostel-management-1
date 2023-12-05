3ced7e5a64c1ec25969e732b40dc1cd176588c74
Isaac Machakata <isaac@caasi.co.zw>
2023-12-05 22:08:18 +0200

    refactor: errors components

--- src/Views/errors.php
+++ src/Views/errors.php
@@ -1,6 +1,6 @@
 <?php if($errors):?>
-<div class="col-11 col-lg-6 px-0">
-    <div class="alert alert-danger border-danger"><?= end($errors) ?></div>
+<div class="col-11 col-lg-6">
+    <div class="alert bg-danger text-white rounded fw-bold text-center"><?= end($errors) ?></div>
 </div>
 <div class="col-12"></div>
 <?php endif;?>
