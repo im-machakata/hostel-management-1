b3127a38d70ec78f2db249d51cb0314b5ca34214
Isaac Machakata <isaac@caasi.co.zw>
2023-12-06 01:05:48 +0200

    refactor: show free bookings

--- public/index.php
+++ public/index.php
@@ -1,6 +1,28 @@
 <?php
+
+use App\System\Database;
+use App\System\Response;
+
 // include autoload file
-require_once __DIR__.'/../src/autoload.php';
+require_once __DIR__ . '/../src/autoload.php';
+
+// if session id is not set, 
+// redirect to login page
+if (!user()->getId()) {
+    Response::redirect('/login.php');
+}
+
+// initiate db
+$db = new Database();
+
+// get available rooms
+$db->prepare('SELECT * FROM rooms WHERE is_booked = :is_booked', [
+    'is_booked' => (int) false
+])->execute();
+$rooms = $db->getRows();
+
+$errors = ['There\'s no free rooms at the moment.'];
+
 render_component('head', ['title' => 'Home']);
 ?>
 
@@ -10,11 +32,14 @@ render_component('head', ['title' => 'Home']);
         <?php render_component('header', ['page' => 'Dashboard']); ?>
         <section class="row justify-content-center">
             <div class="col-12"></div>
-            <?php foreach ($available_rooms as $room) : ?>
+            <?php foreach ($rooms as $room) : ?>
                 <div class="col-sm-6 col-lg-3">
                     <?php render_component('room', $room); ?>
                 </div>
             <?php endforeach; ?>
+
+            <!-- Errors Layouts -->
+            <?php render_component('errors', ['errors' => $errors]); ?>
         </section>
     </main>
     <?php render_component('footer'); ?>
