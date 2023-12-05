9f832fd6dfdc7e6fd55ea40738f76eba80056952
Isaac Machakata <isaac@caasi.co.zw>
2023-12-05 22:08:45 +0200

    refactor: footer

--- src/Views/footer.php
+++ src/Views/footer.php
@@ -1,6 +1,6 @@
-<footer class="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 mt-5 border-top border-success">
+<footer class="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 mt-5">
     <div class="d-flex align-items-center pt-2">
-        <span class="text-muted">Copyright of &copy;<?= SCHOOL_NAME . ', ' . date('Y') ?></span>
+        <span class="text-muted">All Rights Reserved</span>
     </div>
 </footer>
 <script src="/assets/js/bootstrap.bundle.min.js" defer></script>
