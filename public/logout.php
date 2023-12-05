3fc5e77202f7532d0c6d8ad6a60f64eeecd7a155
Isaac Machakata <isaac@caasi.co.zw>
2023-12-05 21:59:22 +0200

    feat: add logout

--- public/logout.php
+++ public/logout.php
@@ -1,8 +1,20 @@
 <?php
+
+use App\System\Response;
 // By initialising the LoginController,
 // it checks it will execute the logout function
 // when it figures that the url is for logging out
 // Nothing more needs to be done as it will redirect the user
 // to the login page even if they were not loged in already
-require "../src/Controllers/LoginController.php";
-$controller = new LoginController();
+// include autoloader
+include __DIR__ . "/../src/autoload.php";
+
+// if session id is set, logout
+if (user()->getId()) {
+    session('user', null);
+}
+
+// redirect to login page
+if (!user()->getId()) {
+    Response::redirect('/login.php');
+}
