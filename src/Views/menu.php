cd70abd896715cf1c1af12c9a1dc7d8b644dd2b6
Isaac Machakata <isaac@caasi.co.zw>
2023-12-06 07:58:20 +0200

    refactor: rename menu items

--- src/Views/menu.php
+++ src/Views/menu.php
@@ -8,15 +8,15 @@
             <ul class="navbar-nav ms-auto">
                 <?php if (!user()->is_student) : ?>
                     <li class="nav-item">
-                        <a class="nav-link<?= url_active('/booked-rooms.php') ?>" href="/booked-rooms.php">View Bookings</a>
+                        <a class="nav-link<?= url_active('/booked-rooms.php') ?>" href="/booked-rooms.php">Bookings</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link<?= url_active('/manage-rooms.php') ?>" href="/manage-rooms.php">Manage Rooms</a>
+                        <a class="nav-link<?= url_active('/manage-rooms.php') ?>" href="/manage-rooms.php">Rooms</a>
                     </li>
                 <?php endif; ?>
                 <?php if (user()->getId()) : ?>
                     <li class="nav-item">
-                        <a class="nav-link<?= url_active('/my-account.php') ?>" href="/my-account.php">My Account</a>
+                        <a class="nav-link<?= url_active('/my-account.php') ?>" href="/my-account.php">Profile</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link<?= url_active('/logout.php') ?>" href="/logout.php">Logout</a>
