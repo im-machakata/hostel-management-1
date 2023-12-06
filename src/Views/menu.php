685947a9e3e164e119df6d55bffff0eecde4039a
Isaac Machakata <isaac@caasi.co.zw>
2023-12-06 06:49:32 +0200

    style: toggle button bg color

--- src/Views/menu.php
+++ src/Views/menu.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top shadow-sm">
     <div class="container-fluid">
         <a class="navbar-brand fw-bold" href="/">Home</a>
-        <button class="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
