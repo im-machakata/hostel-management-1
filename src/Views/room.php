958a4b08d6326ba08b0c4d16b4efa897aa7f4dc0
Isaac Machakata <isaac@caasi.co.zw>
2023-12-06 04:41:50 +0200

    style: room component

--- src/Views/room.php
+++ src/Views/room.php
@@ -1,26 +1,30 @@
-<div class="card border-success">
+<?php
+
+use App\System\Request;
+?>
+<div class="card shadow border-0">
     <div class="ratio ratio-16x9">
         <img src="<?= $image_url ?? ('/assets/images/demo.jpg') ?>" class="card-img-top img-fluid" alt="<?= $name ?> Image">
     </div>
     <div class="card-body">
         <div class="clearfix mb-1">
             <?php if (!Request::isUrl('/booked-rooms.php')) : ?>
-                <div class="text-dark h5 badge bg-warning mb-2">
+                <div class="h5 btn-sm btn btn-primary mb-2">
                     USD $<?= number_format($cost, 2) ?>
                 </div>
             <?php endif;
-            if (session('UserType') == 'Admin' && Request::isUrl('/manage-rooms.php')) : ?>
+            if (!user()->is_student && Request::isUrl('/manage-rooms.php')) : ?>
                 <div class="float-end">
                     <form action="/manage-rooms.php" method="post" class="d-block">
-                        <a class="text-dark h5 badge bg-info mb-2 text-decoration-none" href="#editRoom" data-bs-toggle="modal" data-bs-target="#newRoom" data-id="<?= $id ?>" data-name="<?= $name ?>" data-details="<?= $description ?>" data-cost="<?= $cost ?>" data-image="<?= $image_url ?>" data-booked="<?= $is_booked ?>" data-action="edit">EDIT</a>
+                        <a class="h5 btn-sm btn btn-outline-dark mb-2 text-decoration-none" href="#editRoom" data-bs-toggle="modal" data-bs-target="#newRoom" data-id="<?= $id ?>" data-name="<?= $name ?>" data-details="<?= $description ?>" data-cost="<?= $cost ?>" data-image="<?= $image_url ?>" data-booked="<?= $is_booked ?>" data-action="edit">EDIT</a>
                         <input type="hidden" name="action" value="delete">
                         <input type="hidden" name="id" value="<?= $id ?>">
-                        <button type="submit" class="btn badge bg-danger delete-room">DELETE</button>
+                        <button type="submit" class="btn btn-dark btn-sm mb-2 delete-room">DELETE</button>
                     </form>
                 </div>
             <?php endif; ?>
         </div>
-        <h2 class="card-title"><?= $name ?></h2>
+        <h2 class="card-title h4"><?= $name ?></h2>
         <p class="card-text mb-0"><?= $description ?></p>
         <?php if (Request::isUrl('/booked-rooms.php')) : ?>
             <p class="mb-0">Booked By: <?= $username ?? '<strong>Uknown</strong>' ?></p>
@@ -34,9 +38,9 @@
             </div>
         <?php endif; ?>
         <?php if ($is_booked) : ?>
-            <a href="/book-room.php?id=<?= $id ?>" class="btn btn-success disabled mt-3">Room Is Booked</a>
+            <a href="/book-room.php?id=<?= $id ?>" class="btn btn-primary disabled mt-3">Room Is Booked</a>
         <?php else : ?>
-            <a href="/book-room.php?id=<?= $id ?>" class="btn btn-outline-success mt-3">Book This Room</a>
+            <a href="/book-room.php?id=<?= $id ?>" class="btn btn-outline-dark mt-3">Book This Room</a>
         <?php endif; ?>
     </div>
 </div>
