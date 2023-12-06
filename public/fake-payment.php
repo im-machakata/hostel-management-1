936f410bb898e6bed1ef97dd0a1a58d4e70ba17f
Isaac Machakata <isaac@caasi.co.zw>
2023-12-06 04:59:15 +0200

    feat: approve fake payment

--- public/fake-payment.php
+++ public/fake-payment.php
@@ -1,8 +1,22 @@
 <?php
-require "../src/Controllers/PaymentsController.php";
 
-$controller = new PaymentsController();
+use App\System\Database;
+use App\System\Request;
+use App\System\Response;
 
+// include autoloader
+include __DIR__ . "/../src/autoload.php";
+
+// check if payment id is present
+if (!Request::get('id')) {
+    return Response::redirect('/');
+}
+
+$db = new Database();
+$db->prepare('UPDATE payments SET status = :status WHERE id = :id', [
+    'status' => Request::get('status') ?? 'success',
+    'id' => ''
+])->execute();
 render_component('head', ['title' => 'Fake a payment']); ?>
 
 <body>
@@ -13,25 +27,18 @@ render_component('head', ['title' => 'Fake a payment']); ?>
                 <?php render_component('header', ['page' => 'Fake Payment']); ?>
                 <div class="row justify-content-center mt-4">
                     <!-- Errors Layouts -->
-                    <?php if ($controller->hasErrors()) : ?>
-                        <div class="col-11 col-lg-6">
-                            <div class="alert alert-danger text-lg-center mt-1 mb-4">
-                                <?= $controller->getLastError() ?>
-                            </div>
-                        </div>
-                        <div class="col-12"></div>
-                    <?php endif; ?>
+                    <?php render_component('errors', ['errors' => $errors]); ?>
                     <form action="/fake-payment.php" method="get">
                         <div class="row justify-content-center mb-4">
                             <div class="col-lg-3 mb-3">
                                 <div class="form-floating">
-                                    <input type="text" value="<?= $controller->request->get('id') ?>" placeholder="Payment" id="id" name="id" class="form-control">
+                                    <input type="text" value="<?= Request::get('id') ?>" placeholder="Payment" id="id" name="id" class="form-control">
                                     <label for="id" class="form-label">Payment ID</label>
                                 </div>
                             </div>
                             <div class="col-lg-3 mb-3">
                                 <div class="form-floating">
-                                    <input type="text" value="<?= $controller->request->get('status') ?>" placeholder="Payment" name="status" id="status" class="form-control">
+                                    <input type="text" value="<?= Request::get('status') ?>" placeholder="Payment" name="status" id="status" class="form-control">
                                     <label for="status" class="form-label">Payment Status</label>
                                 </div>
                             </div>
